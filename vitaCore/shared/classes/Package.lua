1c683bc7f6d37a92ad5acb1f60f3a8b5d24721df
PewX <yz_github@pewx.de>
2026-05-13 19:49:54 +0200

    Fixed Package: name buffer too small, unreliable null-byte parsing
    - Increased name read buffer from 64 to 256 bytes to handle long resource paths
    - Replaced gettok with string.find for reliable null-byte delimiter parsing
    - Added stripResourcePath option to store paths without :resource/ prefix
    - Added targetPath parameter to load() for writing files into another resource
    - Added error handling for failed fileCreate/fileOpen calls

--- vitaCore/shared/classes/Package.lua
+++ vitaCore/shared/classes/Package.lua
@@ -1,29 +1,39 @@
 Package = {}
 
-function Package.save(path, filelist)
+function Package.save(path, filelist, stripResourcePath)
     local pack = setmetatable({}, { __index = Package })
     local fh = fileCreate(path)
 
+    if not fh then outputDebugString("[PACKAGE] Cant create package: " .. path) return end
+
     for k, v in pairs(filelist) do
-        pack:addFile(fh, v)
+        pack:addFile(fh, v, stripResourcePath)
     end
 
     fileClose(fh)
 end
 
-function Package.load(path)
+function Package.load(path, targetPath)
     local fh = fileOpen(path)
     while fileGetSize(fh) ~= fileGetPos(fh) do
-        local name = Package._readEntry(fh, 64)
+        local name = Package._readEntry(fh, 256)
         local size = Package._readEntry(fh, 32)
         local data = fileRead(fh, tonumber(size))
-        if fileExists(name) then
-            fileDelete(name)
+        if targetPath then
+            --name = name:gsub("^:[^/]+/", resourceOverwrite .. "/")
+            name = ("%s/%s"):format(targetPath, name)
+            outputDebugString("File: " .. name)
         end
 
+        if fileExists(name) then fileDelete(name) end
+
         local ff = fileCreate(name)
-        fileWrite(ff, data)
-        fileClose(ff)
+        if ff then
+            fileWrite(ff, data)
+            fileClose(ff)
+        else
+            outputDebugString("[PACKAGE] Cant write file: " .. name)
+        end
     end
     fileClose(fh)
 end
@@ -31,21 +41,28 @@ end
 function Package._readEntry(fh, maxlen)
     local pos = fileGetPos(fh)
     local buf = fileRead(fh, maxlen)
-    local name = gettok(buf, 1, "\00")
-    fileSetPos(fh, pos + #name+1)
+    local nullpos = buf:find("\00", 1, true)
+    local name = buf:sub(1, nullpos - 1)
+    fileSetPos(fh, pos + #name + 1)
     return name
 end
 
-function Package:addFile(fh, file)
+function Package:addFile(fh, file, stripResourcePath)
     local r = fileOpen(file)
     if not r then
-        outputDebugString("[PACKAGE] Cant open file : "..file)
+        outputDebugString("[PACKAGE] Cant open file : " .. file)
         return
     end
     local size = fileGetSize(r)
     local data = fileRead(r, size)
     fileClose(r)
-    fileWrite(fh, file)
+
+    local name = file
+    if stripResourcePath then
+        name = file:match("^:[^/]+/(.+)$") or file
+    end
+
+    fileWrite(fh, name)
     fileWrite(fh, "\00")
     fileWrite(fh, tostring(size))
     fileWrite(fh, "\00")
